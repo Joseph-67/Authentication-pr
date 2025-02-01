6331c7e7458b77c0dc0ab7d7778b296802a5b0df
Atuma <carteratuma@gmail.com>
2025-02-01 16:07:13 +0100

    update

--- index.php
+++ index.php
@@ -6,14 +6,9 @@
     <title>Authentication System</title>
     <link rel="stylesheet" href="">
 </head>
-<<<<<<< HEAD
 <body>
     <h2> Welcome</h2>
     <a href="login.php">Login</a>
     <a href="register.php">Register</a>
-=======
-<body>  
-    <a href="register.php">register</a>    
->>>>>>> eeb5ae8d20790b01c1631593e081a1063038630a
 </body>
 </html>
